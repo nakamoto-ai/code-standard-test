4f4392a0516284a80c91a8f546e7cc803211d894
Eddie Ciccone <eddie@nakamotomining.ai>
2024-09-27 13:50:20 -0500

    should pass

--- main.py
+++ main.py
@@ -10,5 +10,6 @@ if __name__ == "__main__":
 
     s += "hola"
 
-    fail = "This will be longer and unaffected by black: adsf asdfas fdas fdas dfas fdas dfa sfd asdf asdf asdf sdf aasd fasdf asdffasff"
+    fail = "This will be longer and unaffected by black: adsf" \
+        "asdfas fdas fdas dfas fdas dfa sfd asdf asdf asdf sdf aasd fasdf asdffasff"
     print(s)
