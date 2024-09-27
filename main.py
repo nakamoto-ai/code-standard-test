8cb177648537fe55c84c09578b6a6407714f20b8
Eddie Ciccone <eddie@nakamotomining.ai>
2024-09-27 13:51:41 -0500

    should pass

--- main.py
+++ main.py
@@ -3,13 +3,16 @@ _summary_
 """
 
 if __name__ == "__main__":
-    s: str = (
+    S: str = (
         "This is just some random text, there will be giberish asdf asdf"
         + " asdf asd fasdf asd fasd fasdf ads fas dfa sdf adsf"
     )
 
-    s += "hola"
+    S += "hola"
 
-    fail = "This will be longer and unaffected by black: adsf" \
-        "asdfas fdas fdas dfas fdas dfa sfd asdf asdf asdf sdf aasd fasdf asdffasff"
-    print(s)
+    FAIL = (
+        "This will be longer and unaffected by black: adsf"
+        "asdfas fdas fdas dfas fdas dfa sfd asdf asdf asdf sdf aasd fasdf"
+        "asdfasdfasdfasdfasdfasdfafdasfdasdfasdfasdfa fafs  asdffasff"
+    )
+    print(S)
